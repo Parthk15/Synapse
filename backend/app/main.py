c5ed40e1f57b641cd546205bc7c339e9ff587894
Parth k <kandiparth@gmail.com>
2026-07-29 21:08:04 +0530

    feat: integrate PostgreSQL and create User model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from app.db.database import Base, engine
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to Synapse API 🚀"}
+    return {
+        "message": "Synapse API is running 🚀"
+    }
